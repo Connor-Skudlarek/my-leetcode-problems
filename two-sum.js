c8be14e01367819a8353a7f015780f2774c1368f
Connor-Skudlarek <connor.skudlarek@gmail.com>
2022-10-23 15:49:41 -0700

    Solved two sum problem

---
+++ two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let check = 0;
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i+1; j < nums.length; j++){
+            check = nums[i] + nums[j]
+            if (check == target){
+                let index1 = i;
+                let index2 = j;
+                return [index1, index2]
+            }
+        }
+    }
+};
