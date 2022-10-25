3dda29dddc14ace782631dcf239d1ca83ceedd64
Connor-Skudlarek <connor.skudlarek@gmail.com>
2022-10-25 08:28:49 -0700

    Changed functions from 'var' to 'let'.

--- JavaScript/running-sum-1d-array.js
+++ JavaScript/running-sum-1d-array.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function (nums) {
+let runningSum = function (nums) {
   for (i = 0; i < nums.length - 1; i++) {
     nums[i + 1] = nums[i + 1] + nums[i];
   }
--- JavaScript/two-sum.js
+++ JavaScript/two-sum.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+let twoSum = function(nums, target) {
     let check = 0;
     for (let i = 0; i < nums.length; i++){
         for (let j = i+1; j < nums.length; j++){
